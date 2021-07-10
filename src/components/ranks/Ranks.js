314c5f8fbad298c772f44d7cf7561c23cc312589
reyhantamang@gmail.com <reyhantamang@gmail.com>
2021-07-10 17:03:46 +0530

    modified

---
+++ src/components/ranks/Ranks.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Ranks = () => {
+  return (
+    <div>
+      <div className="black f3">{"Andrea, your current rank is........"}</div>
+      <div className="red f1">{"#5"}</div>
+    </div>
+  )
+}
+export default Ranks
